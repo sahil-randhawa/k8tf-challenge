d776725c21ec07b523e3b7e709068ff873fae38e
Sahil Randhawa <sahildeep.randhawa@gmail.com>
2025-02-11 01:43:16 -0500

    name mismatch fix

--- cicd-eks-cluster/terraform/modules/vpc/output.tf
+++ cicd-eks-cluster/terraform/modules/vpc/output.tf
@@ -1,7 +1,7 @@
 output "aws_public_subnet" {
-  value = aws_subnet.public_cloudquicklabs_subnet.*.id
+  value = aws_subnet.public_cyderes-sahil-gh_subnet.*.id
 }
 
 output "vpc_id" {
-  value = aws_vpc.cloudquicklabs.id
+  value = aws_vpc.cyderes-sahil-gh.id
 }
