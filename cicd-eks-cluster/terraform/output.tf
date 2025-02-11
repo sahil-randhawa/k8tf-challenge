bd9fcdc79b442c1667c733b1754bfbd37298fa66
Sahil Randhawa <sahildeep.randhawa@gmail.com>
2025-02-11 01:50:13 -0500

    variable name erro fix

--- cicd-eks-cluster/terraform/output.tf
+++ cicd-eks-cluster/terraform/output.tf
@@ -1,11 +1,11 @@
 output "eks_cluster_id" {
   description = "EKS Cluster ID"
-  value = module.eks.eks_cluster_id
+  value = module.eks.cluster_id
 }
 
 output "eks_cluster_endpoint" {
   description = "Endpoint for EKS Control Plane"
-  value = module.eks.eks_cluster_endpoint
+  value = module.eks.cluster_endpoint
 }
 
 output "cluster_name" {
